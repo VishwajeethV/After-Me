22837b5d935e077e6ef06bbc24aa00317c0eaf90
Vishwajeeth V <spacexblue80@gmail.com>
2026-02-19 23:13:32 +0530

    Create pages/index.js for Home/Login page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// Home/Login Page
+
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home/Login Page</h1>
+            <p>Please log in to continue.</p>
+        </div>
+    );
+};
+
+export default HomePage;
