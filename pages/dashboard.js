b9047d82b545764c5b50549f032016e271852cf3
Vishwajeeth V <spacexblue80@gmail.com>
2026-02-19 23:13:43 +0530

    Create dashboard profiles page

---
+++ pages/dashboard.js
@@ -0,0 +1,14 @@
+// Dashboard Profile Page
+
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard Profiles</h1>
+            {/* Add your components here */}
+        </div>
+    );
+};
+
+export default Dashboard;
